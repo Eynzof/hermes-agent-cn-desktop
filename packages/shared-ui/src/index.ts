815a7d44bc21f166a57757fa08daaf13e8a218b6
enzo <eynzof@gmail.com>
2026-06-13 19:53:44 +0800

    refactor: 统一设计系统与设置页组件风格

--- packages/shared-ui/src/index.ts
+++ packages/shared-ui/src/index.ts
@@ -10,5 +10,6 @@ export {
 export type { DensityVariant, ThemeConfig, ThemeVariant } from "./hooks/use-theme";
 export { usePlatform, applyPlatformToDOM } from "./hooks/use-platform";
 export { cn, type ClassValue } from "./utils/cn";
+export * from "./components";
 export * as Dialog from "./composites/dialog";
 export * as Popover from "./composites/popover";
